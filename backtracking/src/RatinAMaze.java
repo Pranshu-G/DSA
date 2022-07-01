79699e75c77283a0e5e7393b4b87b59133a206ad
Pranshu-G <pranshu.grover18@gmail.com>
2022-07-01 23:18:35 +0530

    Rat in a Maze problem

---
+++ backtracking/src/RatinAMaze.java
@@ -0,0 +1,54 @@
+package backtracking.src;
+
+public class RatinAMaze {
+
+    public static void main(String[] args) {
+        // Dummy maze matrix
+        int maze[][] = {{1,1,0},{1,1,0},{1,1,1}};
+
+        boolean answer = ratInaMaze(maze);
+        System.out.println(answer);
+    }
+
+    public static boolean ratInaMaze(int maze[][]){
+        int i,j;
+        int n = maze.length;
+        int path[][] = new int[n][n];
+        return solveMaze(maze, 0 ,0,path);
+
+    }
+
+    public static boolean solveMaze(int maze[][], int i, int j, int path[][]){
+
+        int n = maze.length;
+        //Step 1: Check if the current cell is valid
+        if(i<0 || i > n-1 || j < 0 || j > n-1 || maze[i][j] == 0 || path[i][j]==1){
+            return false;
+        }
+        // Step 2: Add the current cell to path matrix
+        path[i][j] =1;
+        // Step 3: Check if the cell is destination cell
+        if(i == n-1 && j == n-1){
+            return true;
+        }
+        // Step 4: Explore further
+        // a. Top
+        if(solveMaze(maze,i-1,j, path)){
+            return true;
+        }
+        // b. Right
+        if(solveMaze(maze,i, j+1, path)){
+            return true;
+        }
+        // c. Bottom
+        if(solveMaze(maze,i+1, j, path)){
+            return true;
+        }
+        // d. Left
+        if(solveMaze(maze,i, j-1, path)){
+            return true;
+        }
+        // Else return false
+        return false;
+    }
+}
