ea11d0219bd99b81b506eacf397e6251cb7c21b2
Pranshu-G <pranshu.grover18@gmail.com>
2022-10-18 15:03:29 +0530

    string questions

--- Basic-DSA/src/SplitString.java
+++ Basic-DSA/src/SplitString.java
@@ -1,2 +1,35 @@
+import java.util.*;
+
 public class SplitString {
+
+    public static Boolean splitString(String str) {
+        // Write your code here..
+
+        char[] arrVowel = {'a','e','i','o','u'};
+        int n = str.length();
+
+        int countA=0,countB =0;
+        for (int i=0;i<n/2;i++){
+            char a = str.charAt(i);
+            if(a=='a'|| a == 'i'|| a=='o'||a=='e'||a=='u'||a=='A'||a=='E'||a=='I'||a=='O'||a=='U'){
+                countA++;
+            }
+        }
+        for (int j=n/2;j<n;j++){
+            char a = str.charAt(j);
+            if(a=='a'|| a == 'i'|| a=='o'||a=='e'||a=='u'||a=='A'||a=='E'||a=='I'||a=='O'||a=='U'){
+                countB++;
+            }
+        }
+        if(countA==countB){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println(splitString("Aa"));
+    }
 }
--- Basic-DSA/src/reverseString.java
+++ Basic-DSA/src/reverseString.java
@@ -1,2 +1,25 @@
 public class reverseString {
+
+    public static String reverseString(String str) {
+        // Write your code here.
+        StringBuilder sb = new StringBuilder(str);
+        int n = str.length();
+        int j = n-1;
+        for(int i=0;i<n/2;i++){
+            int charEnd = n-i-1;
+            char currentChar = str.charAt(i);
+            sb.setCharAt(i, str.charAt(charEnd));
+            sb.setCharAt(charEnd, currentChar);
+
+
+        }
+        return sb.toString();
+    }
+
+    public static void main(String[] args) {
+        String s = "hello";
+        String ans = reverseString(s);
+
+        System.out.println(ans);
+    }
 }
