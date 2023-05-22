a388dc16e940aa9a7fd30ff20b9ec5e5eb0e422e
Pranshu-G <pranshu.grover18@gmail.com>
2023-05-22 11:25:46 +0530

    Graph

--- Educative/src/Graph/Graph.java
+++ Educative/src/Graph/Graph.java
@@ -11,34 +11,73 @@ public class Graph {
     public Graph(int vertices){
         this.vertices = vertices;
         adjacencyList = new DoublyLinkedListTail[vertices];
+
         for(int i=0;i<vertices;i++){
             adjacencyList[i] = new DoublyLinkedListTail<>();
         }
     }
-    void printGraph(){
-        System.out.println(">>Adjacency list for a directed graph<<");
+
+    public void addEdge(int source, int destination){
+       //for directed graph
+        if(source<vertices && destination<vertices){
+            this.adjacencyList[source].insertAtEnd(destination);
+        }
+//        for undirected graph
+//        this.adjacencyList[destination].insertAtEnd(source);
+    }
+
+    public void printGraph(){
+        System.out.println(">>Adjacency List of Directed Graph<<");
         for(int i=0;i<vertices;i++){
             if(adjacencyList[i]!=null){
                 System.out.print("|" + i + "| => ");
+
                 DoublyLinkedListTail<Integer>.Node temp = adjacencyList[i].getHeadNode();
-                while(temp!=null){
-                    System.out.print("["+ temp.data + "] -> ");
-                    temp = temp.next;
+                while (temp!=null){
+                    System.out.print("[" + temp.data + "]");
+                    temp=temp.next;
                 }
                 System.out.println("null");
-            }
-            else{
-                System.out.println("|" + i + "| => " + "null");
+            } else{
+                System.out.println("|" + i + "| => "+ "null");
             }
         }
-
     }
 
-
-    void addEdge(int source, int destination){
-        if(source<vertices && destination < vertices){
-            this.adjacencyList[source].insertAtEnd(destination);
-        }
-    }
+//    int vertices;
+//    DoublyLinkedListTail<Integer> adjacencyList[];
+//
+//    public Graph(int vertices){
+//        this.vertices = vertices;
+//        adjacencyList = new DoublyLinkedListTail[vertices];
+//        for(int i=0;i<vertices;i++){
+//            adjacencyList[i] = new DoublyLinkedListTail<>();
+//        }
+//    }
+//    void printGraph(){
+//        System.out.println(">>Adjacency list for a directed graph<<");
+//        for(int i=0;i<vertices;i++){
+//            if(adjacencyList[i]!=null){
+//                System.out.print("|" + i + "| => ");
+//                DoublyLinkedListTail<Integer>.Node temp = adjacencyList[i].getHeadNode();
+//                while(temp!=null){
+//                    System.out.print("["+ temp.data + "] -> ");
+//                    temp = temp.next;
+//                }
+//                System.out.println("null");
+//            }
+//            else{
+//                System.out.println("|" + i + "| => " + "null");
+//            }
+//        }
+//
+//    }
+//
+//
+//    void addEdge(int source, int destination){
+//        if(source<vertices && destination < vertices){
+//            this.adjacencyList[source].insertAtEnd(destination);
+//        }
+//    }
 
 }
