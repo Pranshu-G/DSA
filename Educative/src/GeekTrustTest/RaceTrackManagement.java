47a852a653baa749a571c7ae662efd48255748a6
Pranshu-G <pranshu.grover18@gmail.com>
2023-06-14 22:20:47 +0530

    Graph

---
+++ Educative/src/GeekTrustTest/RaceTrackManagement.java
@@ -0,0 +1,22 @@
+package GeekTrustTest;
+
+public class RaceTrackManagement {
+
+    private static String defaultTrack = "Regular";
+    private static String VIP  = "VIP";
+    private static int minBookingHours = 3;
+    private static int extraCharge = 50;
+
+    private static int vehicleCount = 0;
+
+    public static String BOOK(String TYPE_OF_VEHICLE, String VEHICLE_NUMBER,String ENTRY_TIME_IN_HHMM){
+
+
+
+        return "";
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
